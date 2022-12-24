e86f81041bfd4ff484cf07d294c57f00568a8494
Clifford Hilo <cliffordhilo@gmail.com>
2022-12-24 04:16:49 -0800

    changes to doc with suggestions

--- google_cloud__translate.py
+++ google_cloud__translate.py
@@ -39,3 +39,21 @@ def translate_large_text(text, target_language):
   # Concatenate the translations into a single string
   return ''.join(translations)
 
+'''
+If your aim is to produce translations that are intended for human consumption with zero tolerance for mistranslation, then a metric like METEOR (Metric for Evaluation of Translation with Explicit ORdering) or NIST (National Institute of Standards and Technology) may be suitable, as these metrics take into account the order of words in the translation and their semantic meaning.
+Another option might be to use a combination of metrics to get a comprehensive understanding of the performance of the machine learning model. For example, you could use BLEU (Bilingual Evaluation Understudy Score) to measure the overall quality of the translation, and WER (Word Error Rate) or CER (Character Error Rate) to measure the accuracy of the translation.
+It is also worth considering the needs and expectations of the end users of the translation. For example, if the translation is intended for a specific audience or domain (e.g., medical or legal), you may want to use domain-specific metrics to evaluate the quality of the translation.
+Ultimately, the choice of metric will depend on the specific goals and requirements of your application. It is important to carefully consider the purpose of the translation and the needs of the end users when selecting a metric.
+
+
+There are a few different ways you can feed a long list of translations into the Google Cloud Translation API:
+Use the Cloud Translation API client library: The Cloud Translation API client library makes it easy to use the API from your application. You can use the library to send individual translations or batch translations to the API.
+To send individual translations, you can use the translate method of the client library. For example:
+Copy code
+from google.cloud import translate_v2 as translate client = translate.Client() def translate_text(text, target_language): translation = client.translate(text, target_language=target_language) return translation['translatedText']
+To send batch translations, you can use the batch_translate_text method of the client library. For example:
+Copy code
+from google.cloud import translate_v2 as translate client = translate.Client() def translate_text_batch(texts, target_language): translations = client.batch_translate_text( texts, target_language=target_language, source_language='en') return translations
+Use the Cloud Translation API REST API: You can also use the Cloud Translation API REST API to send translations to the API. To do this, you will need to make HTTP POST requests to the API using the /v3beta1/documents:translate endpoint. You can use the source_language_code and target_language_code parameters to specify the source and target languages, and the contents parameter to specify the text to be translated.
+Use the Cloud Translation API command-line interface (CLI): If you prefer to use the command line, you can use the Cloud Translation API CLI to send translations to the API. To do this, you will need to use the gcloud command, along with the translate command and the appropriate flags and arguments. For example:
+'''
